ce3053fef6cf1886ca7385fd5386c7def96243ea
Noah Dietz <ndietz@Noahs-MacBook-Pro.local>
2015-08-28 00:12:08 -0700

    add comments

--- index.js
+++ index.js
@@ -18,10 +18,20 @@ module.exports = {
   getUserByName: getUserByName
 };
 
+/**
+ * initiation function required to handle application client_id
+ * @param  {string} client_id application's client_id
+ */
 function apiInit(client_id){
   this.client_id = client_id;
 }
 
+/**
+ * gets photos of specified user
+ * @param  {string}   userName username of target user
+ * @param  {Function} callback callback called upon completion of API call
+ * @return {object}            array of photos from specified user
+ */
 function getUserPhotos(userName, callback) {
   request({
     url: (HOST + path.join('users', userName, 'photos')),
@@ -40,6 +50,12 @@ function getUserPhotos(userName, callback) {
   });
 }
 
+/**
+ * gets the public info of the specified user
+ * @param  {strong}   userName username of target user
+ * @param  {Function} callback callback called upon completion of API call
+ * @return {object}            specified user object
+ */
 function getUserByName(userName, callback) {
   request({
     url: (HOST + path.join('users', userName)),
